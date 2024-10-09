a74ab02e6abd66b46e57730b6f5333ffe5270956
FarazAhmed <farazahmedcgf@gmail.com>
2024-10-10 00:18:47 +0530

    Basic features created

--- app.js
+++ app.js
@@ -1,21 +1,50 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+let ejs = require("ejs");
 
 const app = express();
 
-app.use("view engine", "ejs");
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let listItem = [];
 
 app.get("/", (req, res) => {
   const today = new Date();
+  const current = today.toLocaleString("en-us", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  });
+  console.log(current);
 
-  if (today.getDate() == 3 || today.getDate() == 4) {
-    res.write("Its a Holiday");
-    console.log("Hold");
-  } else {
-    res.write("BOOO");
-    console.log("Boo");
-  }
-  res.send();
+  let day = "";
+
+  //   switch (current) {
+  //     case 1:
+  //       day = "Monday";
+  //     case 2:
+  //       day = "tuesday";
+  //     case 3:
+  //       day = "wednesday";
+  //     case 4:
+  //       day = "thursday";
+  //     case 5:
+  //       day = "friday";
+  //     case 6:
+  //       day = "saturday";
+  //     case 0:
+  //       day = "sunday";
+  //       break;
+  //     default:
+  //       "Error";
+  //   }
+  res.render("list", { dayName: current, listIt: listItem });
+});
+
+app.post("/", (req, res) => {
+  listItem.push(req.body.form_input);
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
